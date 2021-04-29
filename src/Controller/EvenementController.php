a8498fa00e879bc69b71651b02cb01b2e7293bd4
GICKEL OKABI <gickel.okabiisseke@esprit.tn>
2021-04-29 06:23:40 +0100

    fin

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Evenement;
 use App\Form\EvenementType;
 use App\Repository\EvenementRepository;
+
+
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,15 +28,89 @@ class EvenementController extends AbstractController
         ]);
     }
 
+/*  le methodes pour calendar*/
+
+    /**
+     * @Route("/calendar", name="evenement_calendar", methods={"GET"})
+     */
+    public function calendar(EvenementRepository $evenementRepository): Response
+    {
+
+        $event = $evenementRepository->findAll();
+        $tableau = [];
+
+        foreach ($event as $events){
+            $tableau [] = [
+                "id" => $events->getIdEvenement(),
+                "lien" => $events->getLien(),
+                "image" => $events->getImage(),
+                "start" => $events->getDateEvenement()->format('Y-m-d'),
+                "presentateur" => $events->getPresentateur(),
+                "title" => $events->getTheme()
+            ];
+        }
+
+        $data = json_encode($tableau);
+
+        return $this->render('evenement/calendar.html.twig', compact('data'));
+    }
+
+    /**
+     * @Route("/calendar/{id}/update", name="calendar_update", methods={"PUT"})
+     */
+    public function calendarUpdate(?Evenement $evenement, Request $request): Response
+    {
+        $donnees = json_decode($request->getContent());
+        if(isset($donnees->id) && !empty($donnees->id) &&
+            isset($donnees->lien) && !empty($donnees->lien) &&
+            isset($donnees->date) && !empty($donnees->date) &&
+            isset($donnees->title) && !empty($donnees->title) &&
+            isset($donnees->presentateur) && !empty($donnees->presentateur) &&
+            isset($donnees->image) && !empty($donnees->image)){
+            /*les donner existe*/
+            $code = 200;
+
+            if(!$evenement){
+                $evenement = new Evenement();
+                $code = 201;
+            }
+            $evenement->setImage($donnees->image);
+            $evenement->setLien($donnees->lien);
+            $evenement->setTheme($donnees->title);
+            $evenement->setPresentateur($donnees->presentateur);
+            $evenement->setDateEvenement(new \DateTime($donnees->date));
+
+            $con = $this->getDoctrine()->getManager();
+            $con->persist($evenement);
+            $con->flush();
+
+
+            return new Response('donnees reçu', $code);
+        }
+        else{
+            /*les donnees n'existe pas*/
+            return new response('donner non reçu',404);
+        }
+
+    }
+
+
+    /* les methodes pour colendar*/
+
     /**
      * @Route("/affichage", name="evenement_affichage", methods={"GET"})
      */
-    public function affichage(EvenementRepository $evenementRepository): Response
+    public function affichage(Request  $request, EvenementRepository $evenementRepository, PaginatorInterface $paginator): Response
     {
+
+        $donner = $evenementRepository->findAll();
+        $evnt = $paginator->paginate($donner, $request->query->getInt('page',1),3);
+
         return $this->render('evenement/affichage.html.twig', [
-            'evenements' => $evenementRepository->findAll(),
+            'evenements' => $evnt,
         ]);
     }
+
 
 
     /*testeeeeeeeeeeeeeeeeeeeeee*/
@@ -86,17 +163,40 @@ class EvenementController extends AbstractController
     /**
      * @Route("/prochainEvenementF", name="evenement_prochainF", methods={"GET"})
      */
-    public function evenementProchainsF( EvenementRepository $evenementRepository) : Response
+    public function evenementProchainsF(Request  $request, EvenementRepository $evenementRepository, PaginatorInterface $paginator): Response
+    {
+
+        $donner = $evenementRepository->evenementProhain();
+        $evnt = $paginator->paginate($donner, $request->query->getInt('page',1),3);
+
+        return $this->render('evenement/evenementfront.html.twig', [
+            'evenements' => $evnt,
+        ]);
+    }
+
+    /*public function evenementProchainsF( EvenementRepository $evenementRepository) : Response
     {
         return $this->render('evenement/evenementfront.html.twig', [
             'evenements' => $evenementRepository->evenementProhain(),
         ]);
-    }
+    }*/
 
     /**
      * @Route("/evenementDateF/{dat}", name="evenement_dateF", methods={"GET","POST"})
      */
-    public function evenementDateF( EvenementRepository $evenementRepository) : Response
+    public function evenementDateF(Request  $request, EvenementRepository $evenementRepository, PaginatorInterface $paginator): Response
+    {
+        $date = $_GET['dateR'];
+        $donner = $evenementRepository->evenementDate('"'.$date.'"');
+        $evnt = $paginator->paginate($donner, $request->query->getInt('page',1),3);
+
+        return $this->render('evenement/evenementfront.html.twig', [
+            'evenements' => $evnt,
+        ]);
+    }
+
+
+    /*public function evenementDateF( EvenementRepository $evenementRepository) : Response
     {
 
         $date = $_GET['dateR'];
@@ -105,29 +205,58 @@ class EvenementController extends AbstractController
             'evenements' => $evenementRepository->evenementDate('"'.$date.'"'),
         ]);
 
-    }
+    }*/
 
     /**
      * @Route("/semaineF", name="evenement_semaineF", methods={"GET"})
      */
+    public function evenementSemaineF(Request  $request, EvenementRepository $evenementRepository, PaginatorInterface $paginator): Response
+    {
+
+        $donner = $evenementRepository->evenementSemaine();
+        $evnt = $paginator->paginate($donner, $request->query->getInt('page',1),3);
+
+        return $this->render('evenement/evenementfront.html.twig', [
+            'evenements' => $evnt,
+        ]);
+    }
+
+    /*
+
     public function evenementSemaineF( EvenementRepository $evenementRepository) : Response
     {
         return $this->render('evenement/evenementfront.html.twig', [
             'evenements' => $evenementRepository->evenementSemaine(),
         ]);
-    }
+    }*/
+
+
+
 
     /*font*/
 
     /**
      * @Route("/aujourdhui", name="evenement_aujourdhui", methods={"GET"})
      */
-    public function evenementAujourdhui( EvenementRepository $evenementRepository) : Response
+
+    public function evenementAujourdhui(Request  $request, EvenementRepository $evenementRepository, PaginatorInterface $paginator): Response
+    {
+
+        $donner = $evenementRepository->evenementAujourdhui();
+        $evnt = $paginator->paginate($donner, $request->query->getInt('page',1),3);
+
+        return $this->render('evenement/evenementfront.html.twig', [
+            'evenements' => $evnt,
+        ]);
+    }
+
+
+    /*public function evenementAujourdhui( EvenementRepository $evenementRepository) : Response
     {
         return $this->render('evenement/evenementfront.html.twig', [
             'evenements' => $evenementRepository->evenementAujourdhui(),
         ]);
-    }
+    }*/
 
     /*font*/
 
